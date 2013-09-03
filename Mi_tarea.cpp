76e193c5f1e3264b16d5f77e9e7f0cdfaaeaa348
Ricardo Tapia <RickTC@localhost.localdomain>
2013-09-02 23:16:10 -0400

    segundo commit

--- Mi_tarea.cpp
+++ Mi_tarea.cpp
@@ -7,8 +7,10 @@ main()
 	
  {
 	cout<<"mi primer trabajo de programacion 3"<<endl<<"XD"<<endl;
-	
-	
+		cout<<"realizare una suma"<<endl;
+			int l=2,m=8;
+				cout<<"suma de 2 + 8 ="<<l+m<<endl;
+					cout<<"FIN";
 return EXIT_SUCCESS;
 
  }
